70260ff8b9066512517b6cfb79f83e976d3a581f
amicg <1748425207@qq.com>
2023-12-13 16:15:54 +0800

    tw5

--- vue.config.js
+++ vue.config.js
@@ -1,14 +1,14 @@
 const { defineConfig } = require('@vue/cli-service')
-const { sentryWebpackPlugin } = require("@sentry/webpack-plugin");
+// const { sentryWebpackPlugin } = require("@sentry/webpack-plugin");
 module.exports = defineConfig({
   transpileDependencies: true,
-  devtool: "source-map", // Source map generation must be turned on
-    plugins: [
-      sentryWebpackPlugin({
-        org: Wade,
-        project: vue-sentry,
-        // Auth tokens can be obtained from https://sentry.io/orgredirect/organizations/:orgslug/settings/auth-tokens/
-        authToken: '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',
-      }),
-    ],
+  // devtool: "source-map", // Source map generation must be turned on
+  //   plugins: [
+  //     sentryWebpackPlugin({
+  //       org: Wade,
+  //       project: vue-sentry,
+  //       // Auth tokens can be obtained from https://sentry.io/orgredirect/organizations/:orgslug/settings/auth-tokens/
+  //       authToken: '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',
+  //     }),
+  //   ],
 })
