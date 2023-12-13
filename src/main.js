ac2f5d95cab072aa9d23385218e80c700ec0552d
amicg <1748425207@qq.com>
2023-12-13 15:46:37 +0800

    one

--- src/main.js
+++ src/main.js
@@ -2,8 +2,22 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import * as Sentry from "@sentry/vue";
 
 Vue.config.productionTip = false
+Sentry.init({
+  Vue,
+  dsn: "https://259d4266a31a2bb0e84d61c1a617a3c7@o4506386921619456.ingest.sentry.io/4506386975686657",
+  integrations: [
+    new Sentry.BrowserTracing({
+      routingInstrumentation: Sentry.vueRouterInstrumentation(router),
+      tracePropagationTargets: [],
+    }),
+  ],
+  tracesSampleRate: 1.0,
+  replaysSessionSampleRate: 0.1,
+  replaysOnErrorSampleRate: 1.0,
+});
 
 new Vue({
   router,
