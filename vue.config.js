d01df51fb1ff50a06bfb5a6149e29f82d38a90e7
amicg <1748425207@qq.com>
2023-12-13 16:09:49 +0800

    tw4

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
+const { sentryWebpackPlugin } = require("@sentry/webpack-plugin");
 module.exports = defineConfig({
   transpileDependencies: true,
   configureWebpack: {
