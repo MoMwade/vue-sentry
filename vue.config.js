81e89246f2e86e076131863a0d0f11cb1e59a40d
amicg <1748425207@qq.com>
2023-12-13 16:50:22 +0800

    tw11

--- vue.config.js
+++ vue.config.js
@@ -6,7 +6,7 @@ module.exports = defineConfig({
     devtool: "source-map", // Source map generation must be turned on
     plugins: [
       sentryWebpackPlugin({
-        org: "Wede",
+        org: "wede",
         project: "vue-sentry",
         // Auth tokens can be obtained from https://sentry.io/orgredirect/organizations/:orgslug/settings/auth-tokens/
         authToken: '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',
